260e6dfde9fee52917cb73b8e6ab1d37d20ff5f3
Gelei Chen <chen1474@purdue.edu>
2016-01-10 12:51:57 -0500

    finish chapter 6

--- car_project/spec/car_spec.rb
+++ car_project/spec/car_spec.rb
@@ -1,55 +1,65 @@
-require_relative "car"
+require 'car'
 
 describe 'Car' do
 
-	describe 'attributes' do
-		it "allows reading and writing for :make" do
-			car = Car.new
-			car.make = 'Test'
-			expect(car.make).to eq('Test')
-		end
+  describe 'attributes' do
+    
+    let(:car) { Car.new }
 
-		it "allows reading and writing for :year" do
-			car = Car.new
-			car.year = 9999
-			expect(car.year).to eq(9999)
-		end
+    # "let" is better than "before" for 
+    # setting up instance variables
+    # before(:example) do
+    #   @car = Car.new
+    # end
+    
+    it "allows reading and writing for :make" do
+      car.make = 'Test'
+      expect(car.make).to eq('Test')
+    end
+    
+    it "allows reading and writing for :year" do
+      car.year = 9999
+      expect(car.year).to eq(9999)
+    end
+    
+    it "allows reading and writing for :color" do
+      car.color = 'foo'
+      expect(car.color).to eq('foo')
+    end
+    
+    it "allows reading for :wheels" do
+      expect(car.wheels).to eq(4)
+    end
+    
+    it "allows writing for :doors"
+    
+  end
+  
+  describe '.colors' do
 
-		it "allows reading and writing for :color" do
-			car = Car.new
-			car.color = 'foo'
-			expect(car.color).to eq('foo')
-		end
+    let(:colors) { ['blue', 'black', 'red', 'green'] }
+    
+    it "returns an array of color names" do
+      expect(Car.colors).to match_array(colors)
+    end
 
-		it "allows reading for :wheels" do
-			car = Car.new
-			expect(car.wheels).to eq(4)
-		end
+  end
+  
+  describe '#full_name' do
+    
+    let(:honda) { Car.new(:make => 'Honda', :year => 2004, :color => 'blue') }
+    let(:new_car) { Car.new }
 
-		it "allow writing for :doors"
+    it "returns a string in the expected format" do
+      expect(honda.full_name).to eq('2004 Honda (blue)')
+    end
+    
+    context 'when initialized with no arguments' do
+      it 'returns a string using default values' do
+        expect(new_car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
 
+  end
 
-	end
-
-	describe '.colors' do
-		it "returns an array of color names" do 
-			c = ['blue','black','red','green']
-			expect(Car.colors).to match_array(c)
-		end
-	end
-
-	describe '#full_name' do
-		it "returns a string in the expected format" do
-			honda = Car.new(:make => 'Honda', :year => 2004, :color => 'blue')
-			expect(honda.full_name).to eq('2004 Honda (blue)')
-		end
-
-		context 'when intialized with no arguments' do
-			it 'returns a string using default values' do
-				car = Car.new
-				expect(car.full_name).to eq('2007 Volvo (unknown)')
-			end			
-		end
-	end
-	
 end
